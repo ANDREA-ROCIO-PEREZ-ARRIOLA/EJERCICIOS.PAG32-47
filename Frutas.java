e062760eeef908429a06431879e6bf78a2b45258
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-24 15:28:46 -0600

    Se agrego un comentario a la clase frutas.

--- Frutas.java
+++ Frutas.java
@@ -1,3 +1,9 @@
 public class Frutas {
 
+/*PRIMER EJERCICIO: Desarrollar un programa que permita almacenar 10 nombres de fru
+tas en un vector llamando “frutas” y que posteriormente muestre el 
+contenido del vector pero en forma inversa, es decir, el último ele
+mento ingresado deberá aparecer primero y el primero en el último 
+lugar. */
+
 }
