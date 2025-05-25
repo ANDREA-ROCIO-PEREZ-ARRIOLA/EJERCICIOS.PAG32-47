5f9f996ab32f10433caddc8db4bbc4563c6c8d4f
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-24 22:19:53 -0600

    Se creo la clase Animal y Main para hacer la función de encapsulamiento.

--- EjercicioPag40/Animal.java
+++ EjercicioPag40/Animal.java
@@ -1,5 +1,49 @@
 package EjercicioPag40;
 
 public class Animal {
+/*EJERCICIO 1 Pág.40: Desarrollar una aplicación Java 
+que implemente la clase Animal. 
+Las propiedades que debe tener son: nombre, orden, 
+extremidades debidamente encapsuladas. 
+Para ello, la clase Animal debe ser creada dentro 
+del mismo documento de la clase que la implementará. */
+
+private String nombre;
+private String orden;
+private String extremidades;
+
+public Animal() {
+}
+
+public Animal(String nombre, String orden, String extremidades) {
+    this.nombre = nombre;
+    this.orden = orden;
+    this.extremidades = extremidades;
+}
+
+public String getNombre() {
+    return nombre;
+}
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+public String getOrden() {
+    return orden;
+}
+
+public void setOrden(String orden) {
+    this.orden = orden;
+}
+
+public String getExtremidades() {
+    return extremidades;
+}
+
+public void setExtremidades(String extremidades) {
+    this.extremidades = extremidades;
+}
+
 
 }
---
+++ EjercicioPag40/Main.java
@@ -0,0 +1,16 @@
+package EjercicioPag40;
+
+public class Main {
+    public static void main(String[] args) {
+       Animal animal = new Animal();
+
+       animal.setNombre("Nombre: Perro");
+       animal.setOrden("Orden: Carnivoro");
+       animal.setExtremidades("Extremidades: Cuatro patas");
+       System.out.println(animal.getNombre());
+       System.out.println(animal.getOrden());
+       System.out.println(animal.getExtremidades());
+
+    }
+
+}
