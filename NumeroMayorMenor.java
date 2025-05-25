f1505ad7215df4f7266880de0541ec8810757aca
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-24 19:51:04 -0600

    Se realizo la clase de NumerosMayorMenor con la funcionalidad

--- NumeroMayorMenor.java
+++ NumeroMayorMenor.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class NumeroMayorMenor {
 
-    /*EJERCICIO 4 Pag.32: Elaborar un programa que permita almacenar 10 números cualquiera 
-en un vector llamado “números” y que posteriormente muestre el 
-mayor y el menor valor ingresado. */
+    /*EJERCICIO 4 Pag.32: Elaborar un programa que permita 
+    almacenar 10 números cualquiera en un vector llamado “números” 
+    y que posteriormente muestre el mayor y el menor valor ingresado. */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[10];
+
+        System.out.println("Ingrese 10 números enteros:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Ingrese el número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+        int mayor = numeros[0];
+        int menor = numeros[0];
+        for (int i = 1; i < numeros.length; i++) {
+            if (numeros[i] > mayor) {
+                mayor = numeros[i];
+            }
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+        }
+        System.out.println("El número mayor es: " + mayor);
+        System.out.println("El número menor es: " + menor);
+        scanner.close();
+    }
 }
