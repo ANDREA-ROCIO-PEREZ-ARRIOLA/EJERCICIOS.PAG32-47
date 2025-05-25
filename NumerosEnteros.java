f3ecfca9fb6d92946756a734bfe80b73ee45a29d
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-24 19:30:01 -0600

    Se creo la funcionalidad de la clase NumerosEnteros

--- NumerosEnteros.java
+++ NumerosEnteros.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class NumerosEnteros {
 
-    /*SEGUNDO EJERCICIO: Elaborar un programa que permita almacenar 12 números enteros 
-en un vector llamado “números” y que posteriormente muestre el 
-promedio de los números ingresados. */
+    /*SEGUNDO EJERCICIO: Elaborar un programa que 
+    permita almacenar 12 números enteros en un 
+    vector llamado “números” y que posteriormente muestre el 
+    promedio de los números ingresados. */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[12];
+
+        System.out.println("Ingrese 12 números enteros:");
+
+        for (int i = 0; i < 12; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        int suma = 0;
+        for (int i = 0; i < 12; i++) {
+            suma += numeros[i];
+        }
+        double promedio = (double) suma / 12;
+        System.out.println("El promedio de los números ingresados es: " + promedio);
+        scanner.close();
+
+    }
 
 }
