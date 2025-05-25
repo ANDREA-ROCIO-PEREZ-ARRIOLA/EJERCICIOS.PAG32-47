b92c9a8d432c768d1e2eca554a5081328b09cb9c
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-25 15:23:55 -0600

    Se agrego un comentario a la clase JuegoAnimales

---
+++ EjercicioPag40/JuegoAnimales.java
@@ -0,0 +1,9 @@
+package EjercicioPag40;
+
+public class JuegoAnimales {
+/* 2. Modificar el programa del juego agregando el nivel 2, deben aparecer cuatro preguntas, cada una con un 
+puntaje de 20 si es correcta y de -10 si es incorrecta. Al finalizar el juego, el programa debe mostrar el pun
+taje acumulado de los dos niveles.*/
+
+
+}
