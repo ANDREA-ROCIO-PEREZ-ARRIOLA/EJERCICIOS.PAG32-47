b700741c2e1683acf221cc2d22c34164688a9416
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-25 20:27:37 -0600

    Se creo una clase hija llamada Futbolista de la superclase Integrantes con su funcionalidad

---
+++ EjercicioPag43/Futbolista.java
@@ -0,0 +1,40 @@
+package EjercicioPag43;
+
+public class Futbolista extends Integrantes{
+
+    private int dorsal;
+    private String demarcacion;
+
+    Futbolista() {
+    }
+
+    public Futbolista(int id, String nombre, String apellido, int edad, int dorsal, String demarcacion) {
+        super(id, nombre, apellido, edad);
+        this.dorsal = dorsal;
+        this.demarcacion = demarcacion;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public String getDemarcacion() {
+        return demarcacion;
+    }
+
+    public void setDemarcacion(String demarcacion) {
+        this.demarcacion = demarcacion;
+    }
+
+    public void jugarPartido() {
+        System.out.println("Jugando partido...");
+    }
+
+    public void entrenar() {
+        System.out.println("Entrenando...");
+    }
+}
