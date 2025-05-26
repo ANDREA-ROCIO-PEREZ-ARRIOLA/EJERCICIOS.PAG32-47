470e6592f7082c1d8367b9163f19b323f677a995
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-25 21:16:37 -0600

    Se creo la carpeta d elos ejercicios de la pag.40 con la clase del primer ejercicio llamada vehiculo.

---
+++ EjerciciosPag47/Vehiculo.java
@@ -0,0 +1,9 @@
+package EjerciciosPag47;
+
+public class Vehiculo {
+
+    /*1. Desarrollar un programa en que se defina la clase principal Vehículo y se creen tres clases derivadas de ésta: 
+Automóvil, Barco y Avión; las clases derivadas deberán sobrescribir por lo menos un método de la clase 
+principal utilizando polimorfismo. */
+
+}
